45a5a279232b9dbc9439a610d1ee249fec3c06e8
Emanuele Giaquinta <emanuele.giaquinta@f-secure.com>
2021-04-13 08:15:22 +0300

    Port to FreeBSD and OpenBSD

--- test/tlstest.c
+++ test/tlstest.c
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <netinet/in.h>
 #include <assert.h>
 #include <fsdyn/fsalloc.h>
 #include <async/async.h>
--- test/tlstestserver.c
+++ test/tlstestserver.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <netinet/in.h>
 #include <assert.h>
 #include <fsdyn/fsalloc.h>
 #include <async/async.h>
